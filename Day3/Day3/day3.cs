fe05a42df9c02170e227eab042c85e4edbdbadd7
schilling-p <paulschilling@posteo.de>
2023-07-01 16:26:23 +0200

    added input and wrote requirements

--- Day3/Day3/day3.cs
+++ Day3/Day3/day3.cs
@@ -1,2 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
 
+DAY3 - RUCKSACKS AND COMPARTMENTS
+
+Input:
+    - multiple sequences of characters
+    - each sequence represents 1 Rucksack
+
+Sequence of steps:
+    - take the input and split it in half
+    - find the element that occurs in both halves
+    - for that element, find a priority
+    - add up all the priorities and return the value
+
+Output:
+    - integer that is the sum of the priorities
+
+
+ */
+
+public class Solution
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World from the Main function!");
+    }
+}
