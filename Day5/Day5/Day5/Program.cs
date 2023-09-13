1b04d64a54d990d9ae25bd207881651a1d38b940
Paul Schilling <paulschilling@posteo.de>
2023-09-13 17:37:50 +0200

    inital Day5 commit, use gitignore for old commits

---
+++ Day5/Day5/Day5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Goal 1: Parse the input
+
+// Goal 2: Work with the given puzzle input
+// make a method that takes as input the number of crates, and the stacks from where and to where it shall move them
+// then the method which is my version of the crane can perform the actions
+
+// Goal 3: which crate is at the top of each stack?
+
